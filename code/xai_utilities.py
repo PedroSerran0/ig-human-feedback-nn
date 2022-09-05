77b30956662d51de269f08eac3aedb75884099cd
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-05 18:23:36 +0100

    Recreated code/xai_utilities.py.

--- code/xai_utilities.py
+++ code/xai_utilities.py
@@ -1,61 +1,52 @@
-from sklearn.metrics import accuracy_score
-import torch
-import os
-import torchvision 
-import torch.nn.functional as F
+# Imports
+import io
 import numpy as np
-import sklearn
-from sklearn import model_selection
-from PIL import Image, ImageFilter
+from PIL import Image
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
+
+# PyTorch Imports
+import torch
 import torchvision
 import torchvision.transforms as transforms
-import torchvision.models as models
-import torch.nn as nn
 
 # Captum Imports
-from captum.attr import IntegratedGradients
-from captum.attr import Saliency
-from captum.attr import DeepLift
-from captum.attr import NoiseTunnel
-from captum.attr import GradientShap
-from captum.attr import Occlusion
-from captum.attr import LRP
+from captum.attr import DeepLift, LayerGradCam, LayerAttribution
 from captum.attr import visualization as viz
-from captum.attr import LayerGradCam
-from captum.attr import LayerAttribution
 
-# My Imports
-from CustomDatasets import Aptos19_Dataset
-from ModelArchitectures import PretrainedModel
-from choose_rects import ChooseRectangles
 
-# Plt show function
+
+# Function: Show image
 def imshow(img ,transpose = True):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+
+
+# Function: Convert a Matplotlib figure to a PIL Image and return it
 def fig2img(fig):
-    """Convert a Matplotlib figure to a PIL Image and return it"""
-    import io
+
     buf = io.BytesIO()
     fig.savefig(buf)
     buf.seek(0)
     img = Image.open(buf)
+    
     return img
 
-# Function to get the explanations
-def attribute_image_features(algorithm, input, labels, ind, **kwargs):
-    tensor_attributions = algorithm.attribute(input, target=labels[ind], **kwargs)
+
+
+# Function: Get the explanations
+def attribute_image_features(algorithm, inputs, labels, ind, **kwargs):
+    tensor_attributions = algorithm.attribute(inputs, target=labels[ind], **kwargs)
     
     return tensor_attributions
 
 
+
+# Function: Generate DeepLIFT attributions
 def GenerateDeepLiftAtts(image, model, data_classes, label, temp_pred):
-    #model.eval()
+    # model.eval()
 
     # Get image classification
     model.to('cpu')
@@ -116,6 +107,9 @@ def GenerateDeepLiftAtts(image, model, data_classes, label, temp_pred):
 
     return dl_att, pred
 
+
+
+# Function: Generate DeepLIFT
 def GenerateDeepLift(image, model, data_classes, label):
     model.eval()
 
@@ -144,18 +138,25 @@ def GenerateDeepLift(image, model, data_classes, label):
 
 
     original_image = np.transpose((image.cpu().detach().numpy() / 2) + 0.5, (1, 2, 0))
+    
     # overlayed integrated gradients figure
-    deepLiftFig,_ = viz.visualize_image_attr_multiple(dl_att, original_image, ["original_image","blended_heat_map"],
-                                        ["all","all"],
-                                        show_colorbar=True,
-                                        titles=[f"Original(gt = {data_classes[label]}, predicted = {data_classes[predicted]})", "DeepLift"],
-                                        fig_size=(10, 6))
+    deepLiftFig,_ = viz.visualize_image_attr_multiple(
+        dl_att,
+        original_image,
+        ["original_image","blended_heat_map"],
+        ["all","all"],
+        show_colorbar=True,
+        titles=[f"Original(gt = {data_classes[label]}, predicted = {data_classes[predicted]})", "DeepLift"],
+        fig_size=(10, 6)
+    )
 
 
     return deepLiftFig
 
 
-def GenerateGradCamBatch(data_batch_iteration,batch_it_size, model, data_classes, save_file_dir):
+
+# Function: Generate GradCAM Batch
+def GenerateGradCamBatch(data_batch_iteration, batch_it_size, model, data_classes):
     images, labels = data_batch_iteration.next()
     print('GroundTruth: ', ' '.join('%5s' % data_classes[labels[j]] for j in range(batch_it_size)))
 
@@ -178,9 +179,13 @@ def GenerateGradCamBatch(data_batch_iteration,batch_it_size, model, data_classes
         attributions_lgc = layer_gradcam.attribute(input, target=predicted[ind])
 
         # Visualization of conv layer
-        _ = viz.visualize_image_attr(attributions_lgc[0].cpu().permute(1,2,0).detach().numpy(),
-                                    sign="all",
-                                    title="Last conv layer")
+        _ = viz.visualize_image_attr(
+            attributions_lgc[0].cpu().permute(1,2,0).detach().numpy(),
+            sign="all",
+            title="Last conv layer"
+        )
+
+
         # Upscalling
         upsamp_attr_lgc = LayerAttribution.interpolate(attributions_lgc, input.shape[2:])
 
@@ -189,17 +194,21 @@ def GenerateGradCamBatch(data_batch_iteration,batch_it_size, model, data_classes
         print(image.shape)
 
         # Visualization of attributions
-        _ = viz.visualize_image_attr_multiple(upsamp_attr_lgc[0].cpu().permute(1,2,0).detach().numpy(),
-                                            image.squeeze().permute(1,2,0).numpy(),
-                                            ["original_image","blended_heat_map","masked_image"],
-                                            ["all","positive","positive"],
-                                            show_colorbar=True,
-                                            titles=["Original", "Positive Attribution", "Masked"],
-                                            fig_size=(18, 6))
+        _ = viz.visualize_image_attr_multiple(
+            upsamp_attr_lgc[0].cpu().permute(1,2,0).detach().numpy(),
+            image.squeeze().permute(1,2,0).numpy(),
+            ["original_image","blended_heat_map","masked_image"],
+            ["all","positive","positive"],
+            show_colorbar=True,
+            titles=["Original", "Positive Attribution", "Masked"],
+            fig_size=(18, 6)
+        )
 
- 
+
     return
 
-# Sort aux function: take third element for sort
+
+
+# Function: Sort aux function: take third element for sort
 def takeThird(elem):
     return elem[2]
